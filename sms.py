cd5adbec5d0bdd8360d5dc14264d7f4ce019847a
dignavora8233-sketch <dignavora8233@gmail.com>
2026-07-14 20:25:40 +0530

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,97 @@
+#Student Mengement System
+
+print("Student Mengement System")
+
+students=[
+      {"id":101,"name":'Riva',"score":89},
+      {"id":102,"name":'Mansi',"score":80},
+      {"id":103,"name":'Sakshi',"score":69}
+    ]
+
+print(students)
+
+print("*"*50)
+print("Student name list")
+print("*"*50)
+
+for student in students:
+    print(student["name"])
+
+print("*"*50)
+print("Student Average score")
+print("*"*50)
+
+sum = 0
+for student in students:
+    sum += student['score']
+
+average = sum/3
+
+print("\n Average student score :",average)
+
+print("*"*50)
+print("Add New student")
+print("*"*50)
+
+students.append({
+       "id":104,
+       "name":"Palak",
+       "score":90
+      })
+print(students)
+
+print("*"*50)
+print("Updete Student score")
+print("*"*50)
+
+for student in students:
+    if student['id'] == 103:
+          student['score']=88
+
+print(students)
+
+print("*"*50)
+print("Remove Students from list")
+print("*"*50)
+
+for student in students:
+    if student['name'] == 'Sakshi':
+       students.remove(student)
+       break;
+
+print(students)
+
+print("*"*50)
+print("Students score>80")
+print("*"*50)
+
+for student in students:
+    if student['score']>80:
+        print(student['name'])
+
+print("*"*50)
+print("Sort Decending")
+print("*"*50)
+
+students.sort(key=lambda student:student['score'], reverse=True)
+
+for student in students:
+    print(student['score'],student['name'])
+
+
+print("*"*50)
+print("Higher score")
+print("*"*50)
+
+highest = students[0]
+
+for student in students:
+    if student['score'] > highest['score']:
+        highest = student
+
+print("Name :", highest['name'])
+print("Score:", highest['score'])
+
+
+
+
