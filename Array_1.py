932c5547f093b7ccd524794b73ed2591ed01c983
dignavora8233-sketch <dignavora8233@gmail.com>
2026-07-17 17:28:32 +0530

    Add files via upload

---
+++ Array_1.py
@@ -0,0 +1,21 @@
+# Array in Python
+
+# Create a 1D Array (list) with five integer element.
+
+# 2 Fector of defined in Array
+
+# Homogeneous Array
+
+num = [10 , 20 , 30 , 40 , 50]
+
+print("Array Element:")
+print(type(num))
+
+for i in num:
+    print(i)
+
+# Heterogeneous Array
+
+student = ["Digna" , 17 , 101]
+
+print(student)
